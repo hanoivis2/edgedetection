5def404176ae21303776b414cdb908391eb6b67a
Tran Gia Huy <hanoivis2@gmail.com>
2020-06-06 15:56:03 +0700

    Update control.js

--- control.js
+++ control.js
@@ -19,26 +19,22 @@ document.addEventListener('DOMContentLoaded', function(){
 
 },false);
 
-function draw(v,c,bc,w,h) {
+function draw(v,c,bc,cw,ch) {
     if(v.paused || v.ended) return false;
     // First, draw it into the backing canvas
-    bc.drawImage(v,0,0,w,h);
+    bc.drawImage(v,0,0,cw,ch);
     // Grab the pixel data from the backing canvas
-    var idata = bc.getImageData(0,0,w,h);
+    var idata = bc.getImageData(0,0,cw,ch);
     var data = idata.data;
-    // Loop through the pixels, turning them grayscale
-    for(var i = 0; i < data.length; i+=4) {
-        var r = data[i];
-        var g = data[i+1];
-        var b = data[i+2];
-        var brightness = (3*r+4*g+b)>>>3;
-        data[i] = brightness;
-        data[i+1] = brightness;
-        data[i+2] = brightness;
+    var w = idata.width;
+    var limit = data.length
+    // Loop through the subpixels, convoluting each using an edge-detection matrix.
+    for(var i = 0; i < limit; i++) {
+        if( i%4 == 3 ) continue;
+        data[i] = 127 + 2*data[i] - data[i + 4] - data[i + w*4];
     }
-    idata.data = data;
     // Draw the pixels onto the visible canvas
     c.putImageData(idata,0,0);
     // Start over!
-    setTimeout(function(){ draw(v,c,bc,w,h); }, 0);
+    setTimeout(draw,20,v,c,bc,cw,ch);
 }
