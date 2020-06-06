86d284d8bc4e0ad1d306c625211ab6a177849269
Tran Gia Huy <hanoivis2@gmail.com>
2020-06-06 17:56:20 +0700

    Update control.js

--- control.js
+++ control.js
@@ -31,8 +31,7 @@ function draw(v,c,bc,cw,ch) {
     
     for(var i = 0; i < limit; i++) {
         if( i%4 == 3 ) continue;        //giữ nguyên kênh alpha
-        data[i] = 
-                    (data[i - w*4 -4] + data[i - w*4]*2 +data[i - w*4 + 4]) - (data[i + w*4 -4] + data[i + w*4]*2 +data[i + w*4 + 4])
+        data[i] = data[i] + (data[i - w*4 -4] + data[i - w*4]*2 +data[i - w*4 + 4]) - (data[i + w*4 -4] + data[i + w*4]*2 +data[i + w*4 + 4])
                    + (data[i - 4 - w*4] + data[i - 4]*2 +data[i - 4 + w*4]) - (data[i + 4 - w*4] + data[i + 4]*2 +data[i + 4 + w*4]);
     }
     
